2388f3e9136bed4363c13a0a8775de1dc061529b
snimje <SURENDRA.NIMJE@GMAIL.COM>
2024-12-11 19:18:00 +0530

    Update uuid.sh

--- pcd-ceph/uuid.sh
+++ pcd-ceph/uuid.sh
@@ -2,6 +2,7 @@
 if [ ! -f client.cinder.key ]; then
 	exit
 fi
+apt-get install -y ceph-common
 UUID=013d74e6-4d67-43fe-9ae2-573c2d0124c1
 cat > secret.xml <<EOF
 <secret ephemeral='no' private='no'>
